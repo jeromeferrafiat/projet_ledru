44cb006e51c6fda52d400ac611a7aec1b0208da9
jeromeferrafiat <jeromeferrafiat@aol.com>
2018-02-26 23:12:49 +0100

    ajout des commentaire + correction prop8

--- TD4-OpenJML-2017/src/Explosives.java
+++ TD4-OpenJML-2017/src/Explosives.java
@@ -5,30 +5,44 @@ public class Explosives{
     public String [][] incomp = new String[50][2];
     public int nb_assign = 0;
     public String [][] assign = new String[30][2];
- 
+    
+
+    //Il ne peut pas y avoir moins de 0 incompatibilités, et il ne peut y en avoir plus de 49.
     /*@ public invariant // Prop 1
       @ (0 <= nb_inc && nb_inc < 50);
       @*/
+    
+    //Il ne peut pas y avoir moins de 0 assignations de produits, et il ne peut y en avoir plus de 29.
     /*@ public invariant // Prop 2
       @ (0 <= nb_assign && nb_assign < 30);
       @*/
+    
+    //Pour toute ligne l du tableau des incompatibilité, l[0] et l[1] sont des produits.
     /*@ public invariant // Prop 3
       @ (\forall int i; 0 <= i && i < nb_inc; 
       @         incomp[i][0].startsWith("Prod") && incomp[i][1].startsWith("Prod"));
       @*/
+    
+    //Pour toute ligne l du tableau d'assignations, l[0] est un bâtiment et l[1] est un produit.
     /*@ public invariant // Prop 4
       @ (\forall int i; 0 <= i && i < nb_assign; 
       @         assign[i][0].startsWith("Bat") && assign[i][1].startsWith("Prod"));
       @*/
+    
+    //Un produit ne peut pas être incompaptible avec lui-même.
     /*@ public invariant // Prop 5
       @ (\forall int i; 0 <= i && i < nb_inc; !(incomp[i][0]).equals(incomp[i][1]));
       @*/
+    
+    //Cette propriété sert à vérifier le caractère symétrique de la relation d'incompatibilité. Si a est incompatible avec b, alors b est incompatible avec a.
     /*@ public invariant // Prop 6
       @ (\forall int i; 0 <= i && i < nb_inc; 
       @        (\exists int j; 0 <= j && j < nb_inc; 
       @           (incomp[i][0]).equals(incomp[j][1]) 
       @              && (incomp[j][0]).equals(incomp[i][1]))); 
       @*/
+    
+    //Cette propriété vérifie qu'aucun bâtiment ne contiennent de produits incompatibles entre eux.
     /*@ public invariant // Prop 7
       @ (\forall int i; 0 <= i &&  i < nb_assign; 
       @     (\forall int j; 0 <= j && j < nb_assign; 
@@ -38,14 +52,16 @@ public class Explosives{
       @              || (!(assign[j][1]).equals(incomp[k][1])))));
       @*/
 
-      /*@ public invariant // Prop 8
+    //INVARIANTS AJOUTE PAR LE GROUPE
+    //toutes les lignes du tableau des affectations (assign) sont différentes deux à deux.
+    /*@ public invariant // Prop 8
       @ (\forall int i; 0 <= i &&  i < nb_assign; 
       @     (\forall int j; 0 <= j && j < nb_assign; 
-      @           i != j && (( assign[i][0] != assign[j][0] ) 
-      @             || ( assign[i][1] != assign[j][1] ));
+      @           (i != j && (( assign[i][0] != assign[j][0] ) 
+      @             || ( assign[i][1] != assign[j][1] )));
       @*/
 
-      
+
 
 
   //@ requires nb_inc < 48;  
